2b6378b9e4d2fe205804b4cd1714de528eb78269
Karan2021 <90594223+Karan2021@users.noreply.github.com>
2021-09-15 14:58:45 +0530

    Add files via upload

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of students");
+            int no = Convert.ToInt32(Console.ReadLine());
+            FileStream fs = new FileStream(@"C:\Training\CSharp\FirstConsoleApp\hands_on_1.txt", FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            for (int i = 0; i < no; i++)
+            {
+                Console.WriteLine("Enter the registration number: ");
+                int regno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the course");
+                string course = Console.ReadLine();
+                Console.WriteLine("Enter the course title");
+                string coursetitle = Console.ReadLine();
+                sw.WriteLine("Reg no: {0}", regno);
+                sw.WriteLine("Course: " + course);
+                sw.WriteLine("Course Title: " + coursetitle);
+                sw.WriteLine();
+                sw.WriteLine("***************************************************************************");
+                sw.WriteLine();
+            }
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
